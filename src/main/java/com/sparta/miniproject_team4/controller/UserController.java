231fa82ae5abbc0dca5d0c5483042b7db9f069e8
Geon-OZ <tkfkadlekd10@gmail.com>
2022-06-14 00:05:55 +0900

    회원가입작업중

--- src/main/java/com/sparta/miniproject_team4/controller/UserController.java
+++ src/main/java/com/sparta/miniproject_team4/controller/UserController.java
@@ -1,24 +1,32 @@
 package com.sparta.miniproject_team4.controller;
 
 import com.sparta.miniproject_team4.dto.SignupRequestDto;
+import com.sparta.miniproject_team4.model.Users;
 import com.sparta.miniproject_team4.security.UserDetailsImpl;
 import com.sparta.miniproject_team4.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
 
+    @PostMapping("/users/register")
+    public String registerUser(@Valid SignupRequestDto signupRequestDto, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+             throw new NullPointerException("에러.");
+        }
+        if (!signupRequestDto.getPassword1())
     }
+
 
 //    // 회원 로그인 페이지
 //    @GetMapping("/users/login")
@@ -33,11 +41,11 @@ public class UserController {
 //    }
 
     // 회원 가입 요청 처리
-    @PostMapping("/users/register")
-    public String registerUser(SignupRequestDto requestDto) {
-        userService.registerUser(requestDto);
-        return "/";
-    }
+//    @PostMapping("/users/register")
+//    public Users registerUser(SignupRequestDto requestDto) {
+//        return userService.registerUser(requestDto);
+//
+//    }
 // 유저 로그인 체크(삭제예정_HTML테스트용)
 //    @GetMapping("/api/userCheck")
 //    public String userCheck(@AuthenticationPrincipal UserDetailsImpl userDetails) {
--- src/main/java/com/sparta/miniproject_team4/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/miniproject_team4/dto/SignupRequestDto.java
@@ -2,10 +2,13 @@ package com.sparta.miniproject_team4.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 
 @Setter
 @Getter
 public class SignupRequestDto {
+    @Size(min = 3, max = 25)
+    @NoEmpty
     private String username;
     private String password;
     private String email;
