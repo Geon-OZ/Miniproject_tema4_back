8e10cc2135facedd228ead5c2d7282245e488dec
waryongc <jip95@naver.com>
2022-06-14 01:10:27 +0900

    @Valid 수정사항 추가

--- src/main/java/com/sparta/miniproject_team4/controller/UserController.java
+++ src/main/java/com/sparta/miniproject_team4/controller/UserController.java
@@ -12,6 +12,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+
 @Controller
 @RequiredArgsConstructor
 public class UserController {
