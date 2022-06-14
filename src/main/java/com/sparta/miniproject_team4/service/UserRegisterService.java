e4e5625637266235ca66d5fe29d25d12bd5ae8c7
waryongc <jip95@naver.com>
2022-06-14 12:26:20 +0900

    UserRegisterService 새로 만들어서 이메일에 @들어가있는지 확인하는 부분 넣어놨어용 필요하면 쓰세용

---
+++ src/main/java/com/sparta/miniproject_team4/service/UserRegisterService.java
@@ -0,0 +1,53 @@
+package com.sparta.miniproject_team4.service;
+
+import com.sparta.miniproject_team4.dto.SignupRequestDto;
+import com.sparta.miniproject_team4.model.Users;
+import com.sparta.miniproject_team4.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class UserRegisterService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+
+
+    public Users registerUser(SignupRequestDto requestDto) {
+        // 회원 ID 중복 확인
+        String username = requestDto.getUsername();
+        Optional<Users> found = userRepository.findByUsername(username);
+        if (found.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자 ID 가 존재합니다.");
+        }
+        String email = requestDto.getEmail();
+        Optional<Users> foundEmail = userRepository.findByEmail(email);
+        if (foundEmail.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자 email이 존재합니다.");
+        }
+        if (!email.contains("@")){
+            throw new IllegalArgumentException("이메일 형식을 확인해주세요.");
+        }
+
+        
+        // 패스워드 암호화
+        String password = passwordEncoder.encode(requestDto.getPassword());
+
+        Users users = new Users(username, password, email);
+        userRepository.save(users);
+        return users;
+    }
+
+//    public Users registerUser(String email, String username, String password){
+//        Users users = new Users();
+//        users.setUsername(username);
+//        users.setEmail(email);
+//        users.setPassword(passwordEncoder.encode(password));
+//        this.userRepository.save(users);
+//        return users;
+//    }
+}
